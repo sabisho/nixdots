b2dc5baa31b70e82e3ab96c597041625d800307d
helixoid <ayushrajmth91@gmail.com>
2025-10-31 17:13:46 +0530

    More Modularity!!!

--- configuration.nix
+++ configuration.nix
@@ -7,31 +7,31 @@
     ./modules/editor.nix
     ./modules/fonts.nix
     ./modules/gaming.nix
+    ./modules/laptop-power-management.nix
     ./modules/locale.nix
+    # ./modules/media-server.nix
     ./modules/mpd.nix
+    ./modules/networking.nix
     ./modules/nixgc.nix
     ./modules/nvidia.nix
     ./modules/pipewire.nix
     ./modules/razer.nix
     ./modules/terminal.nix
-    ./modules/video-drivers.nix
-    ./modules/window-manager.nix
-    ./modules/virtualization.nix
-    ./modules/firewall.nix
     ./modules/user-account.nix
-    ./modules/networking.nix
+    ./modules/video-drivers.nix
+    ./modules/virtualization.nix
+    ./modules/window-manager.nix
   ];
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  services.gvfs.enable = true;
-
-  services.gnome.gnome-keyring.enable = true;
+  services = {
+    gvfs.enable = true;
+    gnome.gnome-keyring.enable = true;
+  };
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
