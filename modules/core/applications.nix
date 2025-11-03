e17ad18836390af1bc1baa181e76f93fce657002
helixoid <ayushrajmth91@gmail.com>
2025-11-03 12:53:11 +0530

    Finally fixed all the flake warnings!!!

--- modules/core/applications.nix
+++ modules/core/applications.nix
@@ -7,7 +7,7 @@
     distrobox
     gimp3
     gnome-disk-utility
-    inputs.zen-browser.packages."${system}".default
+    inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default
     meld
     mission-center
     picard
