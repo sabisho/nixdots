4570b0cdbcaf305c6d25fe7e2f8b0f024ec647fc
helixoid <ayushrajmth91@gmail.com>
2026-01-25 21:54:43 +0530

    Added cursor animation options of kitty terminal.

--- modules/home/programs/kitty/config.nix
+++ modules/home/programs/kitty/config.nix
@@ -6,7 +6,9 @@
     };
     themeFile = "GruvboxMaterialDarkHard";
     settings = {
-      cursor_trail = 1;
+      cursor_trail = 3;
+      cursor_trail_start_threshold = 0;
+      cursor_trail_decay = "0.1 0.4";
       background_opacity = 0.8;
     };
   };
