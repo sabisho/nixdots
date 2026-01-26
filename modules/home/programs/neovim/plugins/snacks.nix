f622a641b6e53485e33db09a263fc516785b3e15
helixoid <ayushrajmth91@gmail.com>
2026-01-26 22:03:11 +0530

    Tweaked few configs.

--- modules/home/programs/neovim/plugins/snacks.nix
+++ modules/home/programs/neovim/plugins/snacks.nix
@@ -3,13 +3,10 @@
     plugins = {
       snacks = {
         enable = true;
-
         settings = {
-          # Enable various snacks modules with default settings
           bigfile = {
             enabled = true;
           };
-
           dashboard = {
             enabled = true;
             sections = [
@@ -19,8 +16,6 @@
                 gap = 1;
                 padding = 1;
               }
-              # { section = "lazy"; }
-              # {section = "startup";}
             ];
             preset = {
               header = ''                                                                                   
@@ -33,69 +28,41 @@
                 ██████  █████████████████████ ████ █████ █████ ████ ██████'';
             };
           };
-
           explorer = {
             enabled = true;
           };
-
           indent = {
             enabled = true;
           };
-
           input = {
             enabled = true;
           };
-
           notifier = {
             enabled = true;
             timeout = 3000;
           };
-
           picker = {
             enabled = true;
           };
-
           quickfile = {
             enabled = true;
           };
-
           scope = {
             enabled = true;
           };
-
           scroll = {
             enabled = true;
           };
-
           statuscolumn = {
             enabled = true;
           };
-
           words = {
             enabled = true;
           };
-
-          # Optional: Add styles customization
           styles = {
             notification = {
               # wo.wrap = true;  # Uncomment to wrap notifications
             };
-          };
-        };
-      };
-      noice = {
-        enable = true;
-        settings = {
-          lsp.override = {
-            "vim.lsp.util.convert_input_to_markdown_lines" = true;
-            "vim.lsp.util.stylize_markdown" = true;
-            "cmp.entry.get_documentation" = true;
-          };
-          presets = {
-            bottom_search = true;
-            command_palette = true;
-            long_message_to_split = true;
-            lsp_doc_border = true;
           };
         };
       };
