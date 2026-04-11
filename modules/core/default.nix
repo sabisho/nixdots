4cd5fe663700b9a7c9dd6a0250dc0c72d14386ec
helixoid <ayushrajmth91@gmail.com>
2026-04-11 10:34:36 +0530

    switched to nautilus for file management

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -8,16 +8,17 @@
     ./boot.nix
     ./editor.nix
     ./gamemode.nix
-    ./gamescope.nix
+    # ./gamescope.nix
     ./greetd.nix
     ./locale.nix
     ./localsend.nix
+    ./nautilus.nix
     ./networking.nix
     ./nixos-settings.nix
     ./pipewire.nix
     ./steam.nix
     ./theming.nix
-    ./thunar.nix
+    # ./thunar.nix
     ./user-account.nix
     ./window-manager.nix
   ];
--- modules/core/window-manager.nix
+++ modules/core/window-manager.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}: {
-  programs.niri.enable = true;
-  # programs.hyprland.enable = true;
+  programs.niri = {
+    enable = true;
+    useNautilus = true;
+  };
 
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-gnome
-      # xdg-desktop-portal-hyprland
-      # xdg-desktop-portal-gtk
     ];
   };
 }
