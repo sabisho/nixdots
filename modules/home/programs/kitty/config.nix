afb1a24f11bafabc546c9dd918d6e6b807bdd08c
helixoid <ayushrajmth91@gmail.com>
2026-04-30 16:26:14 +0530

    Some minor adjustments

--- modules/home/programs/kitty/config.nix
+++ modules/home/programs/kitty/config.nix
@@ -8,7 +8,7 @@
       cursor_trail = 3;
       cursor_trail_start_threshold = 0;
       cursor_trail_decay = "0.1 0.4";
-      background_opacity = 0.8;
+      background_opacity = 0.5;
       background_blur = 1;
     };
     # themeFile = "gruvbox-dark-hard";
