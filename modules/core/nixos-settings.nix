858443833f20eb071c31d3d1407b223b128b96fc
helixoid <ayushrajmth91@gmail.com>
2026-06-12 04:32:16 +0530

    few minor tweaks

--- modules/core/nixos-settings.nix
+++ modules/core/nixos-settings.nix
@@ -9,12 +9,12 @@
       "flakes"
     ];
     extra-substituters = [
-      "https://noctalia.cachix.org"
       "https://helix.cachix.org"
+      "https://noctalia.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
       "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
     ];
   };
 
